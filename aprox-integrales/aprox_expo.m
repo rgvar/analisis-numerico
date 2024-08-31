025d49357565a7f78c15653b6e5cd6dd86c4ac2d
Rodrigo <vargasrodrigoluis@gmail.com>
2024-08-31 01:07:40 -0300

    integral primitiva exponencial y modificada aprox integral exponencial

--- aprox-integrales/aprox_expo.m
+++ aprox-integrales/aprox_expo.m
@@ -1,14 +1,14 @@
 # aprox integral función exponencial método trapecios multiple
 
 function aprox_expo(N, p, w)
-    tf = 1;
+    tf = 8;
     t0 = 0;
     dt = (tf - t0) / N;
 
 
-    for k=1:N+2
+    for k=1:N + 2
         tg(k) = (k-1) * dt;
-        yg(k) = e^(p*tg(k)) * sin(w*tg(k));
+        yg(k) = e^(-p*tg(k)) * sin(w*tg(k));
 
     endfor
 
@@ -16,7 +16,7 @@ function aprox_expo(N, p, w)
     It = 0;
     for k=1 : N + 1
         It = It + (yg(k) + yg(k + 1)) * dt/2;
-        # fprintf("k: %d; tg=%.2f, yg=%.4f, It=%.10f\n ", k,tg(k),yg(k),It);
+        #fprintf("k: %d; tg=%.2f, yg=%.4f, It=%.10f\n ", k,tg(k),yg(k),It);
     endfor
 
 
---
+++ aprox-integrales/integral_primitiva.m
@@ -0,0 +1,33 @@
+
+function integral_primitiva(N,w)
+    t0=0;
+    tf=5/w;
+    dt=(tf-t0) / N;
+    dim = N+1;
+
+    for k=1:dim
+        t(k) = (k-1) * dt;
+        y(k) = e^(-w*t(k));
+    endfor
+
+    #{
+	figure(1)
+    plot(t,y,'ob')
+    grid on
+	#}
+	yp(1) = 0;
+	for k=1:N
+		yp(k+1) = yp(k) + dt * (y(k) + y(k+1)) / 2;
+	endfor
+	
+	#{
+	figure(2)
+	plot(t,yp, 'ob')
+	grid on
+	#}
+	format long
+	t = t(dim) 
+	y = y(dim)
+	yp = yp(dim)
+
+endfunction
