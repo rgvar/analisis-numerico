56ed0b9ff3a1499eba72bbc3e38935d97c696740
Rodrigo <vargasrodrigoluis@gmail.com>
2024-09-10 15:59:43 -0300

    aprox derivada seno x dos puntos

---
+++ derivadas/df_dospuntos_seno.m
@@ -0,0 +1,22 @@
+
+
+function df_dospuntos_seno(x = pi/10, h = 0.1)
+
+    dfexacta = cos(x);
+
+    for k=1:5
+    printf("h = %.5f \n", h);
+    df = (sin(x + h) - sin(x)) / h;
+    format long
+    df
+    Er = dfexacta - df;
+    format long
+    Er
+    h = h/10;
+    disp(" ");
+    endfor
+
+
+
+
+endfunction
